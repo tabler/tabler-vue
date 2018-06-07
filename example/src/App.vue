9975d848a20205fe37e67f96210f87008f4a783c
Ivan Vilanculo <vilanculoivan@gmail.com>
2018-06-07 14:45:57 +0200

    pull off navigation items from SiteHeaderComponent

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <site-wrapper>
-    <site-header imageUrl="/static/demo/brand/tabler.svg"></site-header>
-
+    <site-header imageUrl="/static/demo/brand/tabler.svg">
+      <div class="nav-item">
+        <t-button :outline="true" size="sm" href="https://github.com/tabler/tabler-vue"
+                  target="_blank">Source code
+        </t-button>
+      </div>
+      <notifications/>
+      <account-dropdown/>
+    </site-header>
     <site-nav></site-nav>
     <router-view/>
   </site-wrapper>
--- src/components/Site/SiteHeader.vue
+++ src/components/Site/SiteHeader.vue
@@ -7,18 +7,7 @@
                 </slot>
 
                 <div class="d-flex order-lg-2 ml-auto">
-                    <div class="nav-item" v-if="navItems">
-                        <a class="nav-link d-none d-md-flex"></a>
-                        <slot name="nav-items">
-                            <t-button :outline="true" size="sm" href="https://github.com/tabler/tabler-vue"
-                                      target="_blank">Source code
-                            </t-button>
-                        </slot>
-                    </div>
-
-                    <notifications/>
-
-                    <account-dropdown/>
+                    <slot></slot>
                 </div>
                 <a class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse"
                    data-target="#headerMenuCollapse"><span class="header-toggler-icon"></span></a></div>
@@ -27,20 +16,12 @@
 </template>
 
 <script>
-    import Notifications from "../Notifications.vue";
-    import AccountDropdown from "../AccountDropdown.vue";
-
     export default {
         name: "site-header",
         props: {
             imageUrl: {default: '/'},
             href: {default: ''},
             alt: {default: 'Tabler Vue'},
-            navItems: {default: true}
-        },
-        components: {
-            AccountDropdown,
-            Notifications
         }
     }
 </script>
